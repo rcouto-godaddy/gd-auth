dac5afc202456f7f1d66b08cb62492732e4e42f7
Rafael Cabral (rcouto) <124682876+rcouto-godaddy@users.noreply.github.com>
2024-06-26 13:29:20 +0100

    Authentication done but failing

--- gd-auth.py
+++ gd-auth.py
@@ -14,19 +14,34 @@ url_domainbox = "https://live.domainbox.net"
 
 payload = json.dumps({
   "username": "SVCN4if2MQL4Nemzm",
-  "password": "fK2B@sCuns901-?-",
+  "password": get_svc_acct_password("svc-accts.local", "SVCN4if2MQL4Nemzm"),
   "realm": "jomax"
 })
+
 headers = {
-  'Content-Type': 'application/json'
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'X-CSRFToken':  ''
 }
  
 response = requests.post(url_auth_token, headers=headers, data=payload)
-# sso_token = response.json()['data']
-sso_token = response.json()
+sso_token = response.json()['data']
 
-## Example response from SSO API:
-# print(sso_token['data'])
+####################################################################################
 
-pswd = get_svc_acct_password("svc-accts.local", "account")
-pp.pprint(pswd)
+payload2 = [
+  {
+    "device": "lbsa0101-01.mgt.sxb1.gdg",
+    "config_mode": "false",
+    "commands": [
+      "show run"
+    ]
+  }
+]
+
+headers['X-CSRFToken'] = sso_token
+payload2 = json.dumps(payload2)
+resp = requests.request("POST", url_net_commander, headers=headers, data=payload2)
+
+pp.pprint(sso_token)
+pp.pprint(resp)
