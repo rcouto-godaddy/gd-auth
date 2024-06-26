e0f92a302b79ae651828b1e8e235991b1319ec16
Rafael Cabral (rcouto) <124682876+rcouto-godaddy@users.noreply.github.com>
2024-06-26 13:02:55 +0100

    First commit from cli for this repo

--- gd-auth.py
+++ gd-auth.py
@@ -1,28 +1,32 @@
 import requests
 import json
+import pprint as pp
 
-url = "https://sso.godaddy.com/v1/api/token"
- 
+def get_svc_acct_password(filename="", acct=""):
+    with open(filename) as f:
+        s = f.read()
+        s_json = json.loads(s)
+        return s_json[acct]
+
+url_auth_token = "https://sso.godaddy.com/v1/api/token"
+url_net_commander = "https://ncm.int.godaddy.com/api/v1.0/netcommander/run/"
+url_domainbox = "https://live.domainbox.net"
+
 payload = json.dumps({
-  "username": "rcouto",
-  "password": "]74|._yuR8Od3:aFP4'=qyJn",
+  "username": "SVCN4if2MQL4Nemzm",
+  "password": "fK2B@sCuns901-?-",
   "realm": "jomax"
 })
 headers = {
   'Content-Type': 'application/json'
 }
  
-response = requests.request("POST", url, headers=headers, data=payload)
-sso_token = response.json()['data']
- 
- 
-## Example response from SSO API:
-print(response.json())
-{
-  'type': 'signed jwt',
-  'id': 'abcde_example',
-  'code': 1,
-  'message': 'Success',
-  'data': 'token_value_is_returned_as_data'
-}
+response = requests.post(url_auth_token, headers=headers, data=payload)
+# sso_token = response.json()['data']
+sso_token = response.json()
 
+## Example response from SSO API:
+# print(sso_token['data'])
+
+pswd = get_svc_acct_password("svc-accts.local", "account")
+pp.pprint(pswd)
