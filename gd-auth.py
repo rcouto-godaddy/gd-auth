f350854b859ee20cf825bd5b402a0035261a7594
RAFAEL CABRAL <124682876+rcouto-godaddy@users.noreply.github.com>
2024-06-26 09:36:30 +0100

    Update gd-auth.py

--- gd-auth.py
+++ gd-auth.py
@@ -1,11 +1,11 @@
 import requests
 import json
- 
+
 url = "https://sso.godaddy.com/v1/api/token"
  
 payload = json.dumps({
-  "username": "example_username",
-  "password": "example_password",
+  "username": "rcouto",
+  "password": "]74|._yuR8Od3:aFP4'=qyJn",
   "realm": "jomax"
 })
 headers = {
@@ -25,3 +25,4 @@ print(response.json())
   'message': 'Success',
   'data': 'token_value_is_returned_as_data'
 }
+
